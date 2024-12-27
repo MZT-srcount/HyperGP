da35152f8773b229d6e510e1b59c9a979312924b
mazt <cszhitongma@mail.scut.edu.cn>
2024-12-28 01:46:33 +0800

    first commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,9 @@ import pathlib
 
 # path = pathlib.Path(__file__).resolve() / '..' / '..'
 sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.getcwd() + "/../../")
 sys.path.insert(0, os.path.abspath("../"))
-# sys.path.insert(0, os.path.abspath("../../.."))
+sys.path.insert(0, os.path.abspath("../../"))
 # sys.path.insert(0, os.path.abspath('../..'))
 # sys.path.insert(0, os.path.abspath(path))
 
